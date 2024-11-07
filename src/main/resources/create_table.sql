85ce714200d98c8903514b714c0fc01c26bf7954
Eemmmml <tinyStack@Eemmmml.cn>
2024-11-07 21:35:33 +0800

    新增极创工作室招新福利题：wordle后端(含见表语句)

---
+++ src/main/resources/create_table.sql
@@ -0,0 +1,7 @@
+create database if not exists game;
+use game;
+create table if not exists answer(
+    id int primary key auto_increment comment '数据词条的id',
+    answer_date date unique comment '答案的日期',
+    answer_solution varchar(100) comment '答案'
+)
